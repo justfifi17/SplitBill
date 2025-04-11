8ca27a7c5968be2b80a0c839c72ba2a707d9b39b
Fithi Fessehatsion <f2justice16@gmail.com>
2025-04-11 12:01:36 -0700

    Fix: import initialized Firebase admin instance for signup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const admin = require('firebase-admin');
+const admin = require('../config/firebaseAdmin');
 const User = require('../models/User'); // MongoDB user model
 
 /**
