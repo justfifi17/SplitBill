2850a53902e2f76a445f94459ce29e0156a84f55
Fithi Fessehatsion <f2justice16@gmail.com>
2025-04-11 23:48:29 -0700

    updated firebaseAdmin.js

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -5,8 +5,9 @@ admin.initializeApp({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    storageBucket: 'splitbill-8caa8.appspot.com',
+    
   }),
+  storageBucket: 'splitbill-8caa8.appspot.com',
 });
 
 module.exports = admin;
