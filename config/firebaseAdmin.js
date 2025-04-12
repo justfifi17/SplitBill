1c02ac843ef1bf227cafde39fc852b31a19bb970
Fithi Fessehatsion <f2justice16@gmail.com>
2025-04-11 23:42:38 -0700

    Fix bucket

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -5,7 +5,7 @@ admin.initializeApp({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    storageBucket: 'splitbill-8caa8.firebasestorage.app',
+    storageBucket: 'splitbill-8caa8.appspot.com',
   }),
 });
 
